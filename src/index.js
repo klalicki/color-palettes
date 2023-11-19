a955953aa89e81115221a7c7be9a1c1a5d733ca3
Kristoff Lalicki <32229646+klalicki@users.noreply.github.com>
2023-11-19 18:52:35 -0500

    continues to reimplement existing algorithm using colord

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
-import { colord } from "colord";
+import { colord, extend } from "colord";
+import mixPlugin from "colord/plugins/mix";
+
+extend([mixPlugin]);
+
 import "./main.scss";
 console.log(colord);
 
@@ -6,12 +10,16 @@ console.log(colord);
 
 const createPalette = () => {
   const palette = {};
+
+  const accentHueSign = Math.random() > 0.5 ? 1 : -1;
+  const accentHueOffset = (Math.random() * 60 + 100) * accentHueSign;
   palette.accentA = colord({
     h: Math.random() * 360,
-    s: Math.random() * 30 + 70,
-    l: Math.random() * 20 + 45,
+    s: Math.random() * 20 + 80,
+    l: Math.random() * 20 + 40,
   });
-  palette.accentB = palette.accentA.rotate(Math.random() * 60 + 150);
+  palette.accentB = palette.accentA.rotate(accentHueOffset);
+  palette.bg = palette.accentA.mix(palette.accentB).lighten(0.5);
 
   return palette;
 };
